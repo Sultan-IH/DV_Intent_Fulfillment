a9b3260f395b0b6bc2b0dae5aeccb76886b95a45
Sultan Kenjeyev <cerber@Sultans-MacBook-Pro.local>
2019-02-23 15:58:39 +0000

    need home + slack

--- intents/need_home.py
+++ intents/need_home.py
@@ -1,2 +1,7 @@
-print(0)
-print("hi")
+def find_home(location, date, user_id="placeholder"):
+    house = {
+        "location": "68 Hampstead Road, downstairs bathroom",
+        "date": date,
+        "name": "Barbara K"
+    }
+    return house
