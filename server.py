adc6160148b87c4786387e4ed79765dd99e1fd6f
Sultan Kenjeyev <cerber@Sultans-MacBook-Pro.local>
2019-02-24 09:39:34 +0000

    sent text in req context

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 import logging
-from uuid import uuid4 as uuid
 from pprint import pprint
+from uuid import uuid4 as uuid
+
 from flask import Flask, request, jsonify, g
+from twilio.rest import Client
+
+from credentials import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, MENTOR_DEFAULT_NUMBER, TWILIO_DEFAULT_NUMBER
 from db_models import db
 from intents.need_home import find_home
 from sentiment import get_sentiment
-from credentials import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, MENTOR_DEFAULT_NUMBER, TWILIO_DEFAULT_NUMBER
-from twilio.rest import Client
 
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
@@ -27,6 +29,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 db.init_app(app)
 
+
 @app.before_request
 def preprocess():
     logger.info('endpoint: %s, url: %s, path: %s' % (
@@ -42,7 +45,7 @@ def preprocess():
         sentiment = get_sentiment(sent_text, str(g.req_id))
         g.sentiment = sentiment['score']
         print("sentiment: ", g.sentiment)
-        # g.sentiment = get_sentiment()
+        g.query_text = sent_text
 
     except Exception as e:
         print("can't marshall request body to json, got e: ", e)
@@ -55,18 +58,6 @@ def ping_route():
     return jsonify(success=True)
 
 
-@app.route("/pause")
-def pause_route():
-    logger.info("Paused " + str(g.req_id))
-    return jsonify(success=True)
-
-
-@app.route("/message")
-def pause_route():
-    logger.info("Paused " + str(g.req_id))
-    return jsonify(success=True)
-
-
 @app.route("/df_webhook", methods=['POST'])
 def df_webhook():
     print("serving request with id " + str(g.req_id))
@@ -76,6 +67,15 @@ def df_webhook():
     query = g.json['queryResult']
     pprint(query)
     intent = query['intent']['displayName']
+
+    if g.sentiment < 0.01:  # override if fallback
+        print("sending a worry signal")
+        client.messages.create(
+            to=MENTOR_DEFAULT_NUMBER,
+            from_=TWILIO_DEFAULT_NUMBER,
+            body="We think that someone might be at the risk of self harm, their last message was: " + g.query_text
+        )
+
     if intent == 'Urgent Home - yes':
         print("finding a home for " + str(g.req_id))
 
@@ -88,18 +88,6 @@ def df_webhook():
         house = find_home(location, date)
         msg = "we found a house at %s owned by %s to stay at %s" % (house['location'], house['name'], house['date'])
 
-        return jsonify(fulfillmentText=msg)
- 	if action == 'UrgentHome.UrgentHome-yes':
-
-        print("finding a home for " + str(g.req_id))
-        context = query['outputContexts'][0]
-        params = context['parameters']
-
-        date = params['date']
-        location = params['location']['subadmin-area']
-
-        house = find_home(location, date)
-        msg = "we found a house at %s owned by %s to stay at %s" % (house['location'], house['name'], house['date'])
         return jsonify(fulfillmentText=msg)
 
     if intent == "Companionship":
@@ -125,15 +113,14 @@ def df_webhook():
         # if len(mentor_chatrooms) == 0:
         uid = uuid()
         url = "tlk.io/paula-" + str(uid)[:15]
-		message = client.messages.create(
-		    to=MENTOR_DEFAULT_NUMBER, 
-		    from_=TWILIO_DEFAULT_NUMBER,
-		    body="There is a person urgently in need. Please follow on to this chatroom: " + url + "!"
-		    )
+        message = client.messages.create(
+            to=MENTOR_DEFAULT_NUMBER,
+            from_=TWILIO_DEFAULT_NUMBER,
+            body="There is a person urgently in need. Please follow on to this chatroom: " + url + " !"
+        )
 
         msg = "we've created a chatroom " + url + " and we are waiting for a mentor to join, please follow the link"
-	    return jsonify(fulfillmentText=msg)
-
+        return jsonify(fulfillmentText=msg)
 
     return jsonify(g.json)
 
